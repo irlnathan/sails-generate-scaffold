e452f9dd2bf835f3e06946f987b76acb0a6113ac
Travis Webb <me@traviswebb.com>
2014-08-01 02:47:38 -0400

    fixed installer

--- install.js
+++ install.js
@@ -2,10 +2,12 @@ var path = require('path');
 var fs = require('fs');
 var log = require('npmlog');
 
+//console.log(JSON.stringify(module.id, null, 2));
+
 log.info('.sailsrc', 'adding sails-scaffolding generator');
-var sailsrcPath = path.resolve(process.cwd(), '.sailsrc');
+var sailsrcPath = path.resolve(process.cwd(), '../..', '.sailsrc');
 var sailsrc = JSON.parse(fs.readFileSync(sailsrcPath).toString());
 sailsrc.generators.modules.scaffolding = 'sails-scaffolding';
 
-log.info('.sailsrc', 'writing new file...');
+log.info('.sailsrc', 'writing file...');
 fs.writeFileSync(sailsrcPath, JSON.stringify(sailsrc, null, 2));
